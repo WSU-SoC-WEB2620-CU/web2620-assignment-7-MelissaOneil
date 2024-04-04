5e31df4fa6a964639d21c0746b1ffbef83cbf269
Melissa O <97707350+MelissaOneil@users.noreply.github.com>
2024-04-04 01:00:44 -0600

    work on assignment

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,7 @@ require.context('../images/', true, /\.(png|jpg|jpeg|gif|svg)$/i)
 require("../stylesheets/main.scss")
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+//import * as fontawesome from "@fortawesome/fontawesome-free/js/all"
+//add import from fontawesome to use icons
+//import { library, dom } from "@fortawesome/fontawesome-svg-core"
+
